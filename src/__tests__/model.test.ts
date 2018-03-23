f3d5ddd3312ac68a095e97ffa312264335e68fa8
Marshall Bowers <elliott.codes@gmail.com>
2018-03-22 21:34:16 -0400

    Add Model

---
+++ src/__tests__/model.test.ts
@@ -0,0 +1,45 @@
+import { Model } from '../model'
+
+describe('Model', () => {
+  it('creates a model instance', () => {
+    const user = Model({
+      name: 'Johnny Depp',
+      dateOfBirth: new Date('1963-06-09')
+    })
+
+    expect(user).toMatchSnapshot()
+  })
+})
+
+describe('Model.with', () => {
+  it('immutably updates an object', () => {
+    const user = Model({
+      name: 'Brad Pitt'
+    })
+
+    const updatedUser = user.with(user => {
+      user.name = 'Bradley Cooper'
+    })
+
+    expect(user.name).toBe('Brad Pitt')
+    expect(updatedUser.name).toBe('Bradley Cooper')
+  })
+
+  it('is chainable', () => {
+    const post = Model({
+      title: 'Hello, Sailor!',
+      views: 0
+    })
+
+    const incrementViews = post => {
+      post.views++
+    }
+
+    const updatedPost = post
+      .with(incrementViews)
+      .with(incrementViews)
+      .with(incrementViews)
+
+    expect(updatedPost.views).toBe(3)
+  })
+})
---
+++ src/model.ts
@@ -0,0 +1,12 @@
+import produce from 'immer'
+
+interface Model<T> {
+  with: (fn: (x: T) => void) => T & Model<T>
+}
+
+export const Model = <T>(props: T): T & Model<T> => ({
+  ...(props as any),
+  with(transform: (draft: T) => void) {
+    return produce(transform)(this as any)
+  }
+})
