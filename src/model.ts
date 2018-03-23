3aa83482ebb8588a3de89d1b0bb7867e97ea3f9a
Marshall Bowers <elliott.codes@gmail.com>
2018-03-22 23:32:08 -0400

    Update typings to be methods on the object rather than object properties

--- src/model.ts
+++ src/model.ts
@@ -5,13 +5,17 @@ export type Transform<T> = (model: T) => void
 export interface Model<T> {
   /**
    * Runs the given transform and returns a copy of the model after applying the transformation.
+   *
+   * @param transform The transform to apply to the model.
    */
-  with: (transform: Transform<T>) => T & Model<T>
+  with(transform: Transform<T>): T & Model<T>
 
   /**
    * Runs the given transform and mutates the model in-place.
+   *
+   * @param transform The transform to apply to the model.
    */
-  mutate: (transform: Transform<T>) => T & Model<T>
+  mutate(transform: Transform<T>): T & Model<T>
 }
 
 export const Model = <T>(props: T): T & Model<T> => ({
