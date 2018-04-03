591d62e7dff6b01eca1c5d7d1c6902b7a4d32916
Marshall Bowers <elliott.codes@gmail.com>
2018-04-03 13:38:37 -0400

    Add documentation

--- src/lens.ts
+++ src/lens.ts
@@ -2,7 +2,18 @@ import produce from 'immer'
 import { Model, ModelInstance, Transform } from './model'
 
 export interface Lensed<T> {
+  /**
+   * Runs the given transform and returns a copy of the lensed model after applying the transformation.
+   *
+   * @param transform The transform to apply to the lensed model.
+   */
   with(transform: Transform<T>): T & Lensed<T>
+
+  /**
+   * Runs the given transform and mutates the source model in-place.
+   *
+   * @param transform The transform to apply to the source model.
+   */
   withMutations(transform: Transform<T>): T & Lensed<T>
 }
 
